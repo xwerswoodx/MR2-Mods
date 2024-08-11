2ef11bef031e0b0d158cbe41b072e2acd7941bd9
Hamit Devedjioglu <hamitdevecioglu@gmail.com>
2024-08-11 02:22:21 +0100

    Upload index.ts

---
+++ ItemPrice/index.ts
@@ -0,0 +1,20 @@
+// DO NOT MODIFY - Begin
+// Any change to the values provided in MR2 cannot be done without changing
+// the game's version number.
+// This value will be taken from the SDK itself and used as a parameter in-game
+// to make sure the mod's version supports the game's version.
+export const minGameVersion =
+  require("magic-research-2-modding-sdk").minGameVersion;
+// DO NOT MODIFY - End
+
+const {
+  load,
+  preload,
+  id,
+  name,
+  version,
+  description,
+} = require("./ItemPriceMod");
+
+export { description, id, load, name, preload, version };
+
